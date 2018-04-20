d4982abdeb87cbcfe31fa557184628eebef5d89a
ewieczorek <ethantw@iastate.edu>
2018-04-19 22:22:01 -0500

    degree greedy works

--- Main.java
+++ Main.java
@@ -8,10 +8,23 @@ public class Main {
         //tester.add("Iowa State");
         //tester.add("Cyclones");
 
-        WikiCrawler w1 = new WikiCrawler("/wiki/Computer_Science", 100, tester, "output.txt");
+        //WikiCrawler w1 = new WikiCrawler("/wiki/Computer_Science", 100, tester, "output.txt");
+        //w1.crawl();
+
+        WikiCrawler w1 = new WikiCrawler("/wiki/Complexity_theory", 20, tester, "output.txt");
         w1.crawl();
 
-        NetworkInfluence ni = new NetworkInfluence("wikiCC.txt");
-        System.out.println(ni.distance("/wiki/Complex_networks", "/wiki/Warren_Weaver"));
+        NetworkInfluence ni = new NetworkInfluence("output.txt");
+        System.out.println(ni.distance("/wiki/Simplicity", "/wiki/Complexity_economics"));
+        ni.shortestPath("/wiki/Simplicity", "/wiki/Complexity_economics");
+        System.out.println(ni.influence("/wiki/Simplicity"));
+        ArrayList<String> a1 = new ArrayList<>();
+        a1.add("/wiki/Simplicity");
+        System.out.println(ni.influence(a1));
+        ArrayList<String> degreeGreedy = ni.mostInfluentialDegree(5);
+        for(String s: degreeGreedy){
+            System.out.println(s);
+        }
+
     }
 }
--- NetworkInfluence.java
+++ NetworkInfluence.java
@@ -1,3 +1,6 @@
+/**
+ * @author Ethan Wieczorek and Dalton Sherratt
+ */
 // LEAVE THIS FILE IN THE DEFAULT PACKAGE
 //  (i.e., DO NOT add 'package cs311.pa1;' or similar)
 
@@ -7,37 +10,34 @@
 // DO NOT INCLUDE LIBRARIES OUTSIDE OF THE JAVA STANDARD LIBRARY
 //  (i.e., you may only include libraries of the form java.*)
 
-/**
- * @author Hugh Potter
- */
+
+import javafx.util.Pair;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class NetworkInfluence
 {
-    private int edges; //number of edges
-    private int verts;  //number of vertices
-    private int numIter;
+//    private int edges; //number of edges
+//    private int verts;  //number of vertices
+//    private int numIter;
     private HashMap<String, ArrayList<String>> graph;
-    private HashMap<String, ArrayList<String>> UtoV; //out degree of vertices
-    private HashMap<String, ArrayList<String>> VtoU; //in degree of vertices
-    private HashMap<String, Double> influenceGraph;
-    private Set<String> numVerts;        //list of vertices
+    //private HashMap<String, ArrayList<String>> UtoV; //out degree of vertices
+    //private HashMap<String, ArrayList<String>> VtoU; //in degree of vertices
+    //private HashMap<String, Double> influenceGraph;
+    //private Set<String> numVerts;        //list of vertices
     //File name of the graph data
-    private String graphdata;
+    private String graphData;
     //Nodes that have already been checked while computing distance.
-    private Set<String> distanceCheckedNodes;
-    private int lowestDistanceFound;
 
     // NOTE: graphData is an absolute file path that contains graph data, NOT the raw graph data itself
+    //TODO
     public NetworkInfluence(String graphData)
     {
-        this.graphdata = graphData;
+        this.graphData = graphData;
         this.graph = new HashMap<>();
         int fileError = createGraphFromFile();
         if (fileError == 1){
@@ -65,44 +65,49 @@ public class NetworkInfluence
     array list of Strings. First vertex in the path must be u and the last vertex must be v. If there is no
     path from u to v, then this method returns an empty list. The return type is ArrayList<String>
      */
-    //TODO
     public ArrayList<String> shortestPath(String u, String v)
     {
+        LinkedList<String> queue = new LinkedList<>();
+        HashMap<String, ArrayList<String>> distance = new HashMap<>();
+        Set<String> visited = new HashSet<>();
 
+        queue.add(u);
 
-//        private Map<Node, Boolean>> vis = new HashMap<Node, Boolean>();
-//
-//        private Map<Node, Node> prev = new HashMap<Node, Node>();
-//
-//        public ArrayList path(Node start, Node finish){
-//        List directions = new LinkedList();
-//        Queue q = new LinkedList();
-//        Node current = start;
-//        q.add(current);
-//        vis.put(current, true);
-//        while(!q.isEmpty()){
-//            current = q.remove();
-//            if (current.equals(finish)){
-//                break;
-//            }else{
-//                for(Node node : current.getOutNodes()){
-//                    if(!vis.contains(node)){
-//                        q.add(node);
-//                        vis.put(node, true);
-//                        prev.put(node, current);
-//                    }
-//                }
-//            }
-//        }
-//        if (!current.equals(finish)){
-//            System.out.println("can't reach destination");
-//        }
-//        for(Node node = finish; node != null; node = prev.get(node)) {
-//            directions.add(node);
-//        }
-//        path.reverse();
-//        return path;
-//    }
+        visited.add(u);
+
+        ArrayList<String> al1 = new ArrayList<>();
+        al1.add(u);
+        distance.put(u, al1);
+
+        while(queue.size()!=0){
+            String temp = queue.poll();
+            if(graph.get(temp)!=null) {
+                for (String str : graph.get(temp)) {
+                    if (visited.contains(str)) continue;
+
+                    if (distance.containsKey(str)) {
+                        distance.get(str).addAll(distance.get(temp));
+                    } else {
+                        ArrayList<String> al = new ArrayList<>();
+                        al.addAll(distance.get(temp));
+                        if(!distance.get(temp).contains(temp)){
+                            al.add(temp);
+
+                        }
+                        distance.put(str, al);
+                    }
+                    queue.push(str);
+                    visited.add(str);
+                }
+            }
+        }
+        distance.get(v).add(v);
+        if(distance.containsKey(v)) {
+            for(String str: distance.get(v)){
+                System.out.println(str + ",");
+            }
+            return distance.get(v);
+        }
         return null;
     }
 
@@ -111,40 +116,33 @@ public class NetworkInfluence
      */
     public int distance(String u, String v)
     {
-        ArrayList<String> temp = new ArrayList<>();
-        temp.add(v);
-        recDistance(u, v, temp, 0);
-        return checkForNode(u,v,0);
-    }
-
-    private ArrayList<String> recDistance(String startNode, String finalNode, ArrayList<String> nodesToCheck, int distance){
-
-
-
-        ArrayList<String> temp = new ArrayList<>();
-        for(ArrayList<String> al: graph.values()){
-
-        }
-
-        return temp;
-    }
-
-    private int checkForNode(String u, String v, int distance){
-        int distanceFound = 0;
         LinkedList<String> queue = new LinkedList<>();
-        HashMap<String, Integer> visitedNodes = new HashMap<>();
+        HashMap<String, Integer> distance = new HashMap<>();
+        Set<String> visited = new HashSet<>();
 
         queue.add(u);
-        visitedNodes.put(u, 0);
+
+        visited.add(u);
         while(queue.size()!=0){
             String temp = queue.poll();
-            for(int i = 0; i < graph.get(temp).size(); i++){
+            if(graph.get(temp)!=null) {
+                for (String str : graph.get(temp)) {
+                    if (visited.contains(str)) continue;
 
+                    if (distance.containsKey(temp)) {
+                        distance.put(str, distance.get(temp) + 1);
+                    } else {
+                        distance.put(str, 1);
+                    }
+                    queue.push(str);
+                    visited.add(str);
+                }
             }
         }
-
-        return distanceFound;
+        if(distance.containsKey(v)) return distance.get(v);
+        return -1;
     }
+
 
     /*
     distance(ArrayList<String> s, String v). Here s a subset of vertices. This method returns
@@ -152,14 +150,17 @@ public class NetworkInfluence
      */
     public int distance(ArrayList<String> s, String v)
     {
-        int leastDistance = distance(s.get(0), v);
-        for(String str: s){
-            int distance = distance(str, v);
-            if(distance < leastDistance){
-                leastDistance = distance;
+        if(!s.isEmpty()) {
+            int leastDistance = distance(s.get(0), v);
+            for (String str : s) {
+                int distance = distance(str, v);
+                if (distance < leastDistance) {
+                    leastDistance = distance;
+                }
             }
+            return leastDistance;
         }
-        return leastDistance;
+        return -1;
     }
 
     /*
@@ -168,10 +169,32 @@ public class NetworkInfluence
     public float influence(String u)
     {
         // implementation
+        float inf = 0;
+        int[] values = new int[graph.size()];
+        Arrays.fill(values, 0, values.length, 0);
 
-        // replace this:
-        return -1f;
+        for(String str: graph.keySet()){
+            if(!str.equals(u)){
+                int dist = distance(u, str);
+                if(dist != -1) {
+                    values[dist]++;
+                }
+            }
+        }
+
+        for(int i: values){
+            inf += ((1/(Math.pow(2, i))) * values[i]);
+        }
+        return inf;
     }
+
+
+
+    /*
+        float dist = distance(graph.get(Vertex), y)
+     */
+
+
 
     /*
     influence(ArrayList<String> s). Here s holds a set of vertices of G. This method returns
@@ -179,28 +202,79 @@ public class NetworkInfluence
      */
     public float influence(ArrayList<String> s)
     {
-        // implementation
+        float inf = 0;
+        int[] values = new int[graph.size()];
+        Arrays.fill(values, 0, values.length, 0);
 
-        // replace this:
-        return -1f;
+        for(String str: graph.keySet()){
+            int dist = distance(s, str);
+            if(dist != -1) {
+                values[dist]++;
+            }
+        }
+
+        for(int i: values){
+            inf += ((1/(Math.pow(2, i))) * values[i]);
+        }
+        return inf;
     }
 
     /*
     mostInfluentialDegree(int k). Returns a set of k nodes obtained by using Degree Greedy
     algorithm. Return type must be ArrayList<String>.
      */
+    //TODO
     public ArrayList<String> mostInfluentialDegree(int k)
     {
-        // implementation
+        ArrayList<String> output = new ArrayList<>();
+        HashMap<String, Integer> degrees = new HashMap<>();
+        List<Pair<String, Integer>> degree = new ArrayList<>();
+        for(String str: graph.keySet()){
+            degrees.put(str, outDegree(str));
+            int j = 0;
+            if(degree.isEmpty()){
+                degree.add(new Pair<String, Integer>(str, outDegree(str)));
+            }else {
+                boolean added =false;
+                int out = outDegree(str);
+                for (Pair<String, Integer> p : degree) {
+                    if (p.getValue() != null && out >= p.getValue()) {
+                        degree.add(j, new Pair<String, Integer>(str, out));
+                        added = true;
+                        break;
+                    }
+                    j++;
+                }
+                if(!added) degree.add(new Pair<String, Integer>(str, out));
+            }
+        }
 
-        // replace this:
-        return null;
+        for(int i = 0; i < k; i++) {
+            output.add(degree.get(i).getKey());
+        }
+
+
+//
+//
+//        for(int i = degreeList.size(); i > 0; i--) {
+//            if(degreeList.get(i) != null && !degreeList.get(i).isEmpty()){
+//                for(String s: degreeList.get(i)){
+//                    if(output.size() < k){
+//                        output.add(s);
+//                    }else{
+//                        return output;
+//                    }
+//                }
+//            }
+//        }
+        return output;
     }
 
     /*
     mostInfluentialModular(int k). Returns a set of k nodes obtained by using Modular Greedy
     algorithm. Return type must be ArrayList<String>.
      */
+    //TODO
     public ArrayList<String> mostInfluentialModular(int k)
     {
         // implementation
@@ -213,6 +287,7 @@ public class NetworkInfluence
     mostInfluentialSubModular(int k). Returns a set of k nodes obtained by using SubModular
     Greedy algorithm. Return type must be ArrayList<String>.
      */
+    //TODO
     public ArrayList<String> mostInfluentialSubModular(int k)
     {
         // implementation
@@ -223,7 +298,7 @@ public class NetworkInfluence
 
     private int createGraphFromFile(){
         try {
-            BufferedReader br = new BufferedReader(new FileReader(graphdata));
+            BufferedReader br = new BufferedReader(new FileReader(this.graphData));
             String line = "";
             while((line = br.readLine()) != null){
                 if(line.charAt(0) == '/'){
